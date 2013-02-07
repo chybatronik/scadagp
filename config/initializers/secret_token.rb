7eb04760c02791e30491efd1bc3623af908194fe
chybatronik <chybatron@gmail.com>
2013-02-07 14:40:34 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scadagp::Application.config.secret_token = '9966c56554e8741b50b90efa6a1ae934c159b5647ddb1ef2117ee00c0d755a39adbb7b7b3baa7ad47bae654039d864d9b896550aa2f22bf32e00545ade024f4b'
