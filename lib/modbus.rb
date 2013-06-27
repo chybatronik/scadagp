b296579bb4b7aac7c7bd56eea3e3b1acdf5259d5
chybatronik <chybatron@gmail.com>
2013-06-27 11:51:31 +0400

    read data from equipment

--- lib/modbus.rb
+++ lib/modbus.rb
@@ -1,15 +1,43 @@
 require './lib/modbus-cli'
 require 'eventmachine'
 
-cmd = Modbus::Cli::CommandLineRunner.new('modbus-cli') 
+#cmd = Modbus::Cli::CommandLineRunner.new('modbus-cli') 
 
-def func index
-	puts "Tick ..." + index
+class TickCounter
+  attr_reader :start_time, :count
+
+  def initialize
+    reset
+    @tick_loop = EM.tick_loop(method(:tick))
+  end
+
+  def reset
+    @count = 0
+    @start_time = EM.current_time
+  end
+
+  def tick
+    @count += 1
+  end
+
+  def rate
+    @count / (EM.current_time - @start_time)
+  end
 end
-EventMachine.run do
-	10.times do
-    	EM.add_periodic_timer(1) { p cmd.run %w(read 192.168.31.68 %MW355 100) }
-    end
+
+
+period = 1 
+EM.run do
+  counter = TickCounter.new
+  EM.add_periodic_timer(period) do
+    start_time = Time.now
+    cmd = Modbus::Cli::CommandLineRunner.new('modbus-cli') 
+    cmd.run %w(read 192.168.31.68 %MW355 300) 
+    delta = Time.now - start_time
+    puts "srart_time #{start_time} in during #{delta}"
+    counter.reset
+    p '...............................................................'
+  end 
 end
 
 puts "All done."
